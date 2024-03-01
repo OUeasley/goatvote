1664c6553560e6def9ef767fe925ee452c031f97
Bryce Easley <oueasley@gmail.com>
2024-02-29 19:11:11 -0600

    Adding winston logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,28 @@
+const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+
+const customFormat = format.printf(({ level , message, metadata }) => {
+  if (metadata && Object.keys(metadata).length) {
+    return `${level}: ${message} ${JSON.stringify(metadata)}`;
+  }
+  return `${level}: ${message}`;
+});
+export const logger = winston.createLogger({
+  level: 'info', // or any other level
+  format: format.combine(
+    format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] }),
+    customFormat
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(), // Using simple format for console readability
+    }),
+    new winston.transports.Http({
+
+      level: 'info', // Minimum level of logs to send
+      host: 'default.main.great-jarzyna-f1rlbvu.cribl-staging.cloud', // The remote server host
+      port: 20008, // The port on the remote server
+      // You can also specify `ssl` true if you need HTTPS
+    }),
+  ],
+});
