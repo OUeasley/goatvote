2593797e0d72d494d48d7be9a3c63a470422842a
Bryce Easley <oueasley@gmail.com>
2024-03-01 14:39:13 -0600

    Changing port to syslog udp

--- src/lib/metrics.ts
+++ src/lib/metrics.ts
@@ -7,6 +7,6 @@ const metricsErrorHandler = function (error) {
 
 export const metricsClient = new StatsD({
   host: 'default.main.great-jarzyna-f1rlbvu.cribl-staging.cloud',
-  port: 8125,
+  port: 9514,
   errorHandler: metricsErrorHandler
 });
